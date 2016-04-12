159add72816cfad07add86c8fce4675e1c2cd85a
Augustin Mulliez <mulliez.augustin@gmail.com>
2016-04-12 17:48:49 +0200

    new style with brand-color in title + concept

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,7 @@ class PagesController < ApplicationController
 
   def send_request
     RequestMailer.new_request(params).deliver_now
-    flash[:notice] = "Votre demande est envoyée"
+    flash[:notice] = "Votre demande de rêve a bien été envoyée, je vous recontacte trés vite"
     redirect_to root_path
-    flash[:notice] = "Votre demande est envoyée"
   end
 end
